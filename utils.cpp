7cb611d31aab3e7da9b82889b500934486898dbc
Ikmokhirio <Ikmokhirio@mail.ru>
2021-01-06 09:45:49 +0300

    Added function for easier test of file hashing

--- utils.cpp
+++ utils.cpp
@@ -5,6 +5,15 @@
 #include "utils.h"
 
 uint32_t utils::shiftLeft(uint32_t val, uint32_t pos) {
-    uint32_t res=val;
-    return((res<<pos) | (res>>(sizeof(uint32_t)*CHAR_BIT-pos)));
+    uint32_t res = val;
+    return ((res << pos) | (res >> (sizeof(uint32_t) * CHAR_BIT - pos)));
+}
+
+std::string utils::readFileIntoString(const std::string &filepath) {
+    std::ifstream file(filepath, std::ios::binary);
+
+    std::string fileContent((std::istreambuf_iterator<char>(file)),
+                            std::istreambuf_iterator<char>());
+
+    return fileContent;
 }
--- utils.h
+++ utils.h
@@ -7,10 +7,15 @@
 
 #include <climits>
 #include <cstdint>
+#include <string>
+#include <fstream>
+#include <streambuf>
 
 class utils {
 public:
     static uint32_t shiftLeft(uint32_t val, uint32_t pos); // Left Circular Shift
+
+    static std::string readFileIntoString(const std::string &filepath);
 };
 
 
