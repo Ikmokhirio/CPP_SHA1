d51a0e6b7545d46f9156812632fcf68f8e35606b
Ikmokhirio <Ikmokhirio@mail.ru>
2021-01-06 09:46:17 +0300

    Added example of file hashing

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 
 int main() {
 
-    std::string testString = "abcde";
-    HasherSHA h(testString);
+    HasherSHA h;
+    h.hashString(utils::readFileIntoString("test.txt")); // Getting hash from file example
     std::cout << h << std::endl;
-    h.hashString(h.getHashedString());
+
+    h.hashString("abcde"); // Getting hash from string
+    std::cout << h << std::endl;
+
+    h.hashString(h.getHashedString()); // Getting hash from previous hash
     std::cout << h << std::endl;
 
 }
